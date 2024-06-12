8f492785f7c292641e6b38357576f81229849b41
Drako <abhishek491981@gmail.com>
2024-06-12 23:42:08 +0530

    deafult set

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.use("/api/v1", mainRouter);
+app.get('/', (req, res) => {
   res.status(200).json({
     message: "Welcome to the API"
   });
 });
-
-app.use("/api/v1", mainRouter);
 
 
 app.listen(port);
