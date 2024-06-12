e41b5c4fe4b51c5393b37eb327c4aa5030c90160
Drako <abhishek491981@gmail.com>
2024-06-12 23:19:35 +0530

    starting point

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/', (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the API"
+  });
+});
+
 app.use("/api/v1", mainRouter);
 
 
